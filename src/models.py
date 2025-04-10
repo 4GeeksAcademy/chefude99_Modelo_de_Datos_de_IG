18f4df43e9a639afa2d5e26afa0cd030b6092ae5
chefdude99 <chefdude.1999@gmail.com>
2025-04-10 02:38:18 +0000

    errores

--- src/models.py
+++ src/models.py
@@ -1,8 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+class Usuario_instagram(db.Model):
+    tablename = 'usuario'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre_de_usuario: Mapped[str] = mapped_column(
+        String(30), unique=True, nullable=False)
+    nombre: Mapped[str] = mapped_column(String(10), nullable=False)
+    apellido: Mapped[str] = mapped_column(String(15), nullable=False)
+    correo: Mapped[str] = mapped_column(
+        String(50), primary_key=True, unique=True)
+    comentario: Mapped[list['Comentario']
+                  ] = relationship(back_populates='comentario_id')
+    
+
+
+class Comentario(db.Model):
+    tablename = 'comentario'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    texto_comentario: Mapped[str] = mapped_column(String(200), nullable=False)
+    autor_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
+    comentario_id: Mapped['Usuario_instagram'] = relationship(back_populates='comentario')
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
